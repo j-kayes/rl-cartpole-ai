2e776f08777ff42400136feb0636d0b0fd0bf2b4
James Kayes <jkayes2718@gmail.com>
2018-09-02 22:05:28 +0100

    Agent class progress.

--- agent.py
+++ agent.py
@@ -1,20 +1,129 @@
-
 import tensorflow as tf
-class Agent:
-    # This will be used as the Q*(S,A) estimator, the input is the state and actions performed, and the output is the expected reward:
-    input_data = tf.placeholder(tf.float32, [None, 6, 1], name = "x")
+import numpy as np
+import random
 
-    fc1 = tf.layers.dropout(fully_connected(input_data, 128), rate=0.8)
-    fc2 = tf.layers.dropout(fully_connected(fc1, 128), rate=0.8)
-    fc3 = tf.layers.dropout(fully_connected(fc2, 128), rate=0.8)
-    fc4 = tf.layers.dropout(fully_connected(fc3, 128), rate=0.8)
-    fc5 = tf.layers.dropout(fully_connected(fc4, 128), rate=0.8)
+def fully_connected(input_tensor, outputs, activation_func=tf.nn.relu):
+    result = tf.layers.dense(inputs = input_tensor,
+                            units = outputs,
+                            activation = activation_func,
+                            kernel_initializer = tf.contrib.layers.xavier_initializer())
+    return result
 
-    # Output layer represents the expected reward:  
-    output_layer = fully_connected(fc5, 1)
-    def __init__(self):
+class Agent: 
+    
+    def __init__(self, environment, state_size, sequence_length=4, memory_size=100000):
+        self.env = environment
+        self.sequence_length = sequence_length
+        self.state_size = state_size
+
+        self.action_space = np.zeros(shape=(self.env.action_space.n,self.env.action_space.n))
+        n = 0
+        for action in self.action_space:
+            action[n] = 1
+            n += 1
+        # Input size will be the size of the previous sequence/action buffer:
+        self.input_size = sequence_length*(self.env.action_space.n + self.state_size) 
+
         self.memory = []
-        self.state_sequence = []
+        self.memory_size = memory_size
+        self.memory_index = 0
+
+        self.build_model()
         self.sess = tf.Session()
 
-    def deep_q_train(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.sess.close()
+
+    def build_model(self):
+        # This will be used as the Q*(S,A) estimator, the input is the state and actions performed, and the output is the expected reward:
+        self.input_data = tf.placeholder(tf.float32, [None, self.input_size, 1], name = "x")
+        self.fc1 = tf.layers.dropout(fully_connected(self.input_data, 128), rate=0.8)
+        self.fc2 = tf.layers.dropout(fully_connected(self.fc1, 128), rate=0.8)
+        self.fc3 = tf.layers.dropout(fully_connected(self.fc2, 128), rate=0.8)
+        self.fc4 = tf.layers.dropout(fully_connected(self.fc3, 128), rate=0.8)
+        self.fc5 = tf.layers.dropout(fully_connected(self.fc4, 128), rate=0.8)
+
+        # Output layer represents the expected reward:  
+        self.output_layer = fully_connected(self.fc5, 1)
+
+    # Pass in the full list of sequence data and this will preprocess into the p_buffer, so that it can be fed to the graph
+    def process_sequence(self, sequence_data):
+        # Processed sequence will be of the same size as the input, without the final input data:
+        p_size = self.input_size-self.env.action_space.n
+        p_buffer = np.zeros(shape=p_size)
+        i = 0
+        # Loop backwards through p_buffer:
+        while(i < p_size):
+            if(i < len(sequence_data)):
+                i += 1
+                p_buffer[p_size-i] = sequence_data[len(sequence_data)-i]
+            else:
+                # For the sake of simplicity, I will fill the remaining buffer with 0's:
+                for index in range(p_size - i):
+                    p_buffer[index] = 0
+                i = p_size
+
+        return p_buffer
+
+    def get_output(self, x_input):
+        return self.sess.run(self.output_layer, feed_dict={self.input_data: x_input})
+
+    # Loop over possible actions to find the action with the best expected reward acording to the model:
+    def get_best_action(self, sequence_data):
+        best_action = None
+        best_reward = None
+        for action in self.action_space:
+            # Join with processed state/action sequence data:
+            x_input = np.concatenate(self.process_sequence(sequence_data), action)
+            # Feed into graph, determine reward:
+            exp_reward = self.get_output(x_input)
+            if((best_reward is None) or (exp_reward > best_reward)):
+                best_reward = exp_reward
+                best_action = action
+        return best_action
+
+    # This will append the memory with state/action/reward data:
+    def get_samples(self, stop_after_limit=True, n_games=500000, max_t=250, epsilon=1.0):
+        game_counter = 0
+        for game in range(n_games):
+            game_counter += 1
+            initial_state = self.env.reset()
+            sequence = [] # Sequence of states
+            sequence.extend(initial_state)
+            # Auto-reset after this:
+            for t in range(max_t):
+                action = None
+                processed_current_state = self.process_sequence(sequence)
+                if(random.random() < epsilon):
+                    action = self.env.action_space.sample()
+                else:
+                    # Best action for this processed sequence(acording to the model):
+                    action = self.get_best_action(processed_current_state)
+                # Get the reward/state information after taking this action:
+                next_state, reward, done, infom = self.env.step(action)
+                con_array = np.concatenate((self.action_space[action], next_state))
+                sequence.extend(con_array.tolist())
+                processed_next_state = self.process_sequence(sequence)
+                # Append to memory (up to limit):
+                if(len(self.memory) < self.memory_size):     
+                    self.memory.append((processed_current_state, action, reward, processed_next_state))
+                elif(self.memory_index < self.memory_size):
+                    # Overwrite from the beginning(when full):
+                    self.memory[self.memory_index] = (processed_current_state, action, reward, processed_next_state)
+                    self.memory_index += 1
+                else:
+                    self.memory[0] = (processed_current_state, action, reward, processed_next_state)
+                    self.memory_index = 1
+
+                if(done): # Game over
+                    break
+            
+            if(stop_after_limit):
+                print('After game {} memory buffer {}% full'.format(game_counter, 100.0*len(self.memory)/self.memory_size))
+                if(len(self.memory) >= self.memory_size):
+                    break 
+            else:
+                print('Game {} of {} complete, memory buffer {}% full'.format(game_counter, n_games, 100.0*len(self.memory)/self.memory_size))
--- cartpole-learner.py
+++ cartpole-learner.py
@@ -4,87 +4,15 @@ import numpy as np
 import tensorflow as tf
 from statistics import mean, median
 from collections import Counter
+from agent import *
 
 lr = 1e-3
 gamma = 0.95 # Discount rate
-env = gym.make('CartPole-v0') 
-env.reset()
+env = gym.make('CartPole-v0')
+state_len = env.observation_space.shape[0] 
 
-# Run n games with a randomly generated input, and record the games that just so happen to be above some threshold:
-def random_games(n_games = 1000, score_threshold = 40, goal_steps = 200):
-    training_data = []
-    scores  = []
-    accepted_scores = []
+with Agent(env, state_len) as agent:
+    print('Getting samples...')
+    agent.get_samples(10000)
 
-    for game in range(n_games):
-        score = 0
-        game_memory = []
-        current_state = []
-        for step in range(goal_steps):
-            random_var = random.randrange(0,2) # 0 or 1
-            action = random_var  # Random action, needs to be 0 or 1 for env.step
-            next_state, reward, done, info = env.step(action) # Perform the action and record data
-            # Observation/prev_observation contains the 4 variables used to represent the state of the game:
-            if(len(current_state) > 0):
-                # Append the list with previous state/observation and the current action performed that will lead to the new observation
-                action = [1, 0] if random_var else [0, 1] # Update action with array of possibilities
-                game_memory.append((current_state, action, reward, next_state, done)) 
-
-            current_state = next_state
-            score += reward
-
-            if done:
-                break
-        
-        scores.append(score)
-        # Successful games get added to the training data:
-        if(score >= score_threshold):
-            accepted_scores.append(score)
-            for step_data in game_memory:
-                training_data.append(step_data)
-        env.reset()
-    
-    print('Average accepted score:', mean(accepted_scores))
-    print('Median score for accepted scores:', median(accepted_scores))
-    print('Number of accepted scores:', len(accepted_scores))
-    print(Counter(accepted_scores))
-
-    # After all random games, we return the data from those games that met the threshold for training on:
-    return training_data
-
-def fully_connected(input_tensor, outputs, activation_func=tf.nn.relu):
-    result = tf.layers.dense(inputs = input_tensor,
-                            units = outputs,
-                            activation = activation_func,
-                            kernel_initializer = tf.contrib.layers.xavier_initializer())
-    return result
-
-def get_output(session, network, input_data):
-    return session.run(network, feed_dict={input_data: input_data})
-
-def get_best_reward(session, network, state_data):
-    outputs = []
-    for action in range(2):
-        action = [1, 0] if action else [0, 1]
-        x_input = state_data + action # Concatenate the two lists to provide the full S,a input needed.
-        outputs.append(get_output(session, network, x_input))
-    return max(outputs)
-
-def train_model(session, training_data, batch_size, network, epochs=5, saved_model=False):
-    for epoch in range(epochs):
-        minibatch = random.sample(training_data, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            if(not done):
-                # Reward + discounted best reward for the next state:
-                target = (reward + gamma*get_best_reward(session, network, next_state))
-            else:
-                target = reward
-            x_input = state + action
-            cost = tf.squared_difference(target, get_output(session, network, x_input))
-            optimizer = tf.train.AdamOptimizer(lr).minimize(cost)
-            session.run(optimizer, feed_dict={input_data: x_input})
-
-
-with tf.Session() as sess:
-    best_games_data = random_games()
-    train_model(sess, best_games_data, int(len(best_games_data)*0.2), output_layer)
+print('Done!')
