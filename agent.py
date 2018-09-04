cc463ac81bfd723d3af1b59c2365ed1c08b641e8
James Kayes <jkayes2718@gmail.com>
2018-09-04 19:36:24 +0100

    Saved model will  get a mean score of over 199.5 over a 100+ sample.

--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import random
-from statistics import mean
+from statistics import mean, median
 
 def fully_connected(input_tensor, outputs, activation_func=tf.nn.relu):
     result = tf.layers.dense(inputs = input_tensor,
@@ -12,7 +12,7 @@ def fully_connected(input_tensor, outputs, activation_func=tf.nn.relu):
 
 class Agent: 
     
-    def __init__(self, environment, state_size, learning_rate, sequence_length=4, memory_size=100000):
+    def __init__(self, environment, state_size, learning_rate=1e-4, sequence_length=10, memory_size=100000):
         self.env = environment
         self.sequence_length = sequence_length
         self.state_size = state_size
@@ -32,6 +32,8 @@ class Agent:
         self.lr = learning_rate
         
         self.build_model()
+        # For saving and loading the graph after training:
+        self.saver = tf.train.Saver(save_relative_paths=True)
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
         
@@ -41,15 +43,15 @@ class Agent:
     def __exit__(self, exc_type, exc_value, traceback):
         self.sess.close()
 
-    def build_model(self, fc_layers=4, layer_connections=128, d_rate=1.0):
+    def build_model(self, fc_layers=4, layer_connections=128, hold_rate=1.0):
         # This will be used as the Q*(S,A) estimator:
         self.input_data = tf.placeholder(tf.float32, [None, self.input_size], name = "x")
         self.target = tf.placeholder(tf.float32, [None], name = "y")
 
         self.fc_layer = []
-        self.fc_layer.append(tf.layers.dropout(fully_connected(self.input_data, layer_connections), rate=d_rate))
+        self.fc_layer.append(tf.layers.dropout(fully_connected(self.input_data, layer_connections), rate=hold_rate))
         for layer in range(fc_layers):
-            self.fc_layer.append(tf.layers.dropout(fully_connected(self.fc_layer[layer-1], layer_connections), rate=d_rate))
+            self.fc_layer.append(tf.layers.dropout(fully_connected(self.fc_layer[layer-1], layer_connections), rate=hold_rate))
         
         # Output layer represents the expected reward for each possible action:  
         self.output_layer = fully_connected(self.fc_layer[fc_layers-1], self.env.action_space.n)
@@ -61,7 +63,6 @@ class Agent:
         self.out_actions = []
         self.loss = []
         self.optimize = [] 
-        # TODO: try cleaning this up a bit:
         # Seperate loss/optimisation for each action:
         i = 0
         for action_q in tf.split(self.output_layer, num_or_size_splits=self.env.action_space.n, axis=1):
@@ -69,8 +70,6 @@ class Agent:
             self.loss.append(tf.squared_difference(self.target, action_q))
             self.optimize.append(tf.train.AdamOptimizer(self.lr).minimize(self.loss[i]))
             i += 1
-
-        
 
     # Pass in the full list of sequence data and this will preprocess into the p_buffer, so that it can be fed to the graph
     def process_sequence(self, sequence_data):
@@ -103,7 +102,7 @@ class Agent:
         return q_value
 
     # This will append the memory with state/action/reward data and return the average score across games:
-    def get_samples(self, stop_after_limit=True, n_games=500000, max_t=250, epsilon=1.0):
+    def get_samples(self, stop_after_limit=True, n_games=500000, max_t=250, epsilon=1.0, display_frames=False):
         game_counter = 0
         scores = []
         frames = 0
@@ -125,6 +124,8 @@ class Agent:
                     action = self.get_best_action(processed_current_state)
                 # Get the reward/state information after taking this action:
                 next_state, reward, done, infom = self.env.step(action)
+                if(display_frames):
+                    self.env.render()
                 score += reward
                 if(not done):
                     sequence.extend(np.concatenate((self.action_space[action], next_state)).tolist()) 
@@ -150,21 +151,29 @@ class Agent:
         return mean(scores), frames
         
     # This will attempt to train the graph:
-    def train_network(self, games=10000, batch_size=32, initial_epsilon=1.0, final_epsilon=0.05, epsilon_frames_range=75000, gamma=0.95):
-        while(len(self.memory) < batch_size): # Play randomly until memmory has at least batch_size entries
+    def train_network(self, target_mean_score = 185.1, games=200000, batch_size=32, initial_epsilon=1.0, final_epsilon=0.1, epsilon_frames_range=100000, gamma=0.95):
+        while(len(self.memory) < batch_size): # Play randomly until memory has at least batch_size entries
             self.get_samples(False, 1, epsilon=1.0) 
         total_frames = 0
-        # Scales linearly from initial to final epsilon up to epsilon frames range:
+        scores = []
         for game in range(games):
+            # Scales linearly from initial to final epsilon up to epsilon frames range:
             if(total_frames < epsilon_frames_range):
                 e = initial_epsilon - ((initial_epsilon - final_epsilon)*(total_frames/epsilon_frames_range))
             else:
                 e = final_epsilon
             score, frames = self.get_samples(False, 1, epsilon=e) # Play a random game, and record data to memory buffer
+            scores.append(score)
+            m_score = mean(scores)
+            if(len(scores) == 250):
+                scores = []
             total_frames += frames
-            print('Game: {} Score: {} Frames: {} e: {}'.format(game, score, total_frames, e))
+            print('Game: {} Score: {}, Mean: {:.2f} Frames: {} e: {:.4f}'.format(game, score, m_score, total_frames, e))
+            
+            if((m_score >= target_mean_score) and (len(scores) > 100)):
+                print('Target mean score reached')
+                break
             mini_batch = random.sample(self.memory, batch_size)
-        
             for p_state, action, reward, p_next_state, done in mini_batch:
                 target = reward
                 if(not done):
@@ -172,7 +181,26 @@ class Agent:
                 
                 # Train:
                 # Seperate optimize for each loss function/action:
-                # TODO: Why would this lead to a 0 q-value for either action: Learning rate needed adjusting
-                # Check iff removing randomness improves average score.
                 self.sess.run(self.optimize[action], feed_dict={self.input_data: p_state, self.target: [target]})
-        
+        print('Training complete')
+        save_path = self.saver.save(self.sess, 'saved/model.ckpt')
+        print('Model saved: {}'.format(save_path))
+
+    def load_model(self, path):
+        print('Attempting to load previously saved model...')
+        self.saver.restore(self.sess, path)
+
+    # Play and record scores with the currently loaded graph:     
+    def play(self, games=100, epsilo=0.05, show_game=True):
+        scores = []
+        total_frames = 0
+        for game in range(games):
+            game_num = game+1
+            score, frames = self.get_samples(False, 1, epsilon=epsilo, display_frames=show_game)
+            scores.append(score)
+            m_score = mean(scores)
+            med_score = median(scores)
+
+            total_frames += frames
+            print('Games: {} Score: {}, Mean Score: {:.2f}, Median: {:.2f} Total Frames: {}'.format(game_num, score, m_score, med_score, total_frames))
+
--- cartpole-learner.py
+++ cartpole-learner.py
@@ -4,11 +4,12 @@ import numpy as np
 import tensorflow as tf
 from agent import *
 
-lr = 5e-5
 env = gym.make('CartPole-v0')
-state_len = env.observation_space.shape[0] 
+state_size = env.observation_space.shape[0] 
 
-with Agent(env, state_len, lr) as agent:
-    agent.train_network()
+with Agent(env, state_size) as agent:
+    #agent.train_network()
+    agent.load_model('saved/model.ckpt')
+    agent.play(100, epsilo=0.0)
 
 print('Done!')
