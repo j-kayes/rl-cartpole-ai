0cd0ed619202d67fdf584ef77d24fb4583d8fc35
James Kayes <31102962+j-kayes@users.noreply.github.com>
2019-02-25 17:50:36 +0000

    Update cartpole-learner.py

--- cartpole-learner.py
+++ cartpole-learner.py
@@ -4,12 +4,13 @@ import numpy as np
 import tensorflow as tf
 from agent import *
 
-env = gym.make('CartPole-v0')
-state_size = env.observation_space.shape[0] 
+if __name__ == "__main__":
+    env = gym.make('CartPole-v0')
+    state_size = env.observation_space.shape[0] 
 
-with Agent(env, state_size) as agent:
-    #agent.train_network()
-    agent.load_model('saved/model.ckpt')
-    agent.play(100, epsilo=0.0)
+    with Agent(env, state_size) as agent:
+        #agent.train_network()
+        agent.load_model('saved/model.ckpt')
+        agent.play(100, epsilo=0.0)
 
-print('Done!')
+    print('Done!')
